c5912af0b767dc8bd22b89f09a37bb0d4268518d
Asrigum54 <asriimbuhg0708@gmail.com>
2025-06-24 14:33:10 +0700

    update database file

--- app/Database/Migrations/2025-06-24-062113_Database.php
+++ app/Database/Migrations/2025-06-24-062113_Database.php
@@ -4,15 +4,41 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class Database extends Migration
+class Resepsionis extends Migration
 {
     public function up()
     {
+        $this->forge->addField([
+            'resepsionis_id' => [
+                'type' => 'INT',
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type' =>'varchar',
+                'constraint' => 100,
+            ],
+            'email' => [
+                'type' => 'varchar',
+                'constraint' => 100,
+            ],
+            'no_hp' => [
+                'type' => 'varchar',
+                'constraint' => 20,
+            ],
+            'password' => [
+                'type' => 'varchar',
+                'constraint' => 10,
+            ],
+
+        ]);
+        $this->forge->addKey('resepsionis_id', true);
+        $this->forge->createTable('resepsionis');
         //
     }
 
     public function down()
     {
-        //
+      $this->forge->dropTable('resepsionis');  //
     }
 }
+
